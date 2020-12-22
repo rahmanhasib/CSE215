bb6fd0d64f12c6526565a266c2288d16ef287626
rahmanhasib <76481332+rahmanhasib@users.noreply.github.com>
2020-12-22 20:32:37 +0600

    Create Task1.java

---
+++ Lab2/Task1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        switch(num1%2){
+        case 0:
+           System.out.println(num1+" is an Even number");
+            break;
+        case 1:
+          System.out.println(num2+" is an Odd number");
+    }
+}
