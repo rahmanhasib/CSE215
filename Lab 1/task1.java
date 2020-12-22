7b96c08d99658e3fe28355b70f59de84b0e23541
rahmanhasib <76481332+rahmanhasib@users.noreply.github.com>
2020-12-22 20:25:09 +0600

    Update task1.java

--- Lab 1/task1.java
+++ Lab 1/task1.java
@@ -1,1 +1,10 @@
 
+public class Task1 {
+ 
+ public static void main(String[] args) {
+  System.out.println("Name: Hasibur Rahman");
+   System.out.println("Age: 23");
+   System.out.println("Department: CSE");
+ }
+ 
+}
